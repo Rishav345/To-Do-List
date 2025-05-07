1570537d35e011b71639adb80aba0fcd71c8481a
Rishav Verma <166117840+Rishav345@users.noreply.github.com>
2025-05-07 10:28:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+const SearchBox = document.getElementById("textarea");
+const Container = document.getElementById("list-container")
+
+const button = document.getElementById("btn");
+
+button.addEventListener("click",()=>{
+    if (SearchBox.value === '') {
+        alert("You Must Write something");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = SearchBox.value;
+        Container.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span)
+    }
+    SearchBox.value = "";
+    saveData();
+})
+SearchBox.addEventListener("keydown",(e)=> {
+    if(e.key == "Enter"){
+        if (SearchBox.value === '') {
+            alert("You Must Write something");
+        }
+        else {
+            let li = document.createElement("li");
+            li.innerHTML = SearchBox.value;
+            Container.appendChild(li);
+            let span = document.createElement("span");
+            span.innerHTML = "\u00d7";
+            li.appendChild(span)
+        }
+        SearchBox.value = "";
+        saveData();
+    }
+});
+
+// function AddTask() {
+//     if (SearchBox.value === '') {
+//         alert("You Must Write something");
+//     }
+//     else {
+//         let li = document.createElement("li");
+//         li.innerHTML = SearchBox.value;
+//         Container.appendChild(li);
+//         let span = document.createElement("span");
+//         span.innerHTML = "\u00d7";
+//         li.appendChild(span)
+//     }
+//     SearchBox.value = "";
+//     saveData();
+// }
+
+
+
+Container.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData()
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", Container.innerHTML);
+}
+
+function ShowData() {
+    Container.innerHTML = localStorage.getItem("data");
+}
+ShowData();
